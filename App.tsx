0cfba955754d21f34c0639326c3bf6c1568ffbaf
mbuthi <mbuthimungai34@gmail.com>
2025-04-17 12:26:34 +0300

    feat(redux): Configure store, add user reducers and selectors

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { Provider } from "react-redux";
 import AuthNavigation from "./app/navigation/AuthNavigation";
+import store from "./app/state/store";
 export default function App() {
   return (
-    <NavigationContainer>
-      <SafeAreaProvider>
-        <SafeAreaView style={styles.container}>
-          <AuthNavigation />
-        </SafeAreaView>
-      </SafeAreaProvider>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <SafeAreaView style={styles.container}>
+            <AuthNavigation />
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
